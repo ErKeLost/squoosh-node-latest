d16d8cd8f2d5be4dd08952801a4a1f54176c125e
erkelost <1256029807@qq.com>
2024-11-15 14:42:47 +0800

    chore: support all node version

---
+++ playground/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), squoosh()],
+});
+
+function squoosh() {
+  return {
+    name: 'squoosh',
+    load(id) {},
+    buildStart() {
+      let SquooshPool;
+      import('squoosh-next')
+        .then((module) => {
+          SquooshPool = module.ImagePool;
+          delete globalThis.navigator;
+          console.log(SquooshPool);
+        })
+        .catch(console.error);
+    },
+  };
+}
